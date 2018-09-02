6c526a48384c7680d4ee5832b6e2f2dc6d486cb9
千夜 <1342746626@qq.com>
2018-09-02 13:29:49 +0800

    基本页面提交，菜单界面及闪屏

---
+++ app/src/main/java/com/example/airforce/HaoYouActivity.java
@@ -0,0 +1,13 @@
+package com.example.airforce;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HaoYouActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hao_you);
+    }
+}
---
+++ app/src/main/java/com/example/airforce/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.example.airforce;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        initView();
+
+    }
+
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            finish();
+        }
+    };
+
+
+    private void initView() {
+        handler.sendEmptyMessageDelayed(1, 2000);
+    }
+
+
+}
